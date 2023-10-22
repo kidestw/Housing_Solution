7717e76493a39bb7806aa1ccf0e0e4785f40ca44
kidestw <kidestworku2321@gmail.com>
2023-10-22 17:40:06 +0300

    edited pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,6 +4,7 @@ export default function About() {
   return (
     <div className='py-20 px-4 max-w-6xl mx-auto'>
       <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Our Housing Webapp</h1>
+      <p className='mb-4 text-slate-700'>By Kidest and Meryem for Nedamco Academy Project-Phase2.</p>
       <p className='mb-4 text-slate-700'>This Housing webapp is a platform provided for helping clients buy, sell, and rent properties in the most desirable neighborhoods and meet owners directly. We are making  house rental, buying and selling process as smooth as possible.</p>
       <p className='mb-4 text-slate-700'>
       Our mission is to help our users achieve their housing goals by letting them find their desired asset directly and easily every step of the way.
